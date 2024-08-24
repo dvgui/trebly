f73c1d7016bf0ee6018007ab4f3009a2f8daee03
Fede Rascon <federascon@gmail.com>
2024-08-24 20:41:35 -0300

    actualizacion

--- app/oldPage/page.tsx
+++ app/oldPage/page.tsx
@@ -1,12 +1,13 @@
-// import { PayBlock } from "@/components/Pay";
+import { PayBlock } from "@/components/Pay";
 import { SignIn } from "@/components/SignIn";
 import { VerifyBlock } from "@/components/Verify";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-y-3">
-      <VerifyBlock />
       <SignIn />
+      <VerifyBlock />
+      <PayBlock />
     </main>
   );
 }
